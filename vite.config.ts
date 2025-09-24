2116dca81c12a44946331bf1ca521b6df7c96d37
Lily <lily@example.com>
2025-09-24 09:03:47 +0800

    V2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/AIpiao/',
   plugins: [react()],
+  server: {
+    open: '/AIpiao/',
+  },
+  preview: {
+    open: '/AIpiao/',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
